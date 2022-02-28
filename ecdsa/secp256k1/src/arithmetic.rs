1a9fc228624c184841c0fe73a7f463bfb2c31b05
David Nevado <david@davidnevado.xyz>
2022-02-28 17:01:34 +0100

    Remove unused `Group` trait
    This trait is imported from `halo2wrong` instead

--- ecdsa/secp256k1/src/arithmetic.rs
+++ ecdsa/secp256k1/src/arithmetic.rs
@@ -7,26 +7,3 @@
 mod fields;
 
 pub(crate) use fields::*;
-
-use halo2wrong::halo2::arithmetic::FieldExt;
-
-/// This represents an element of a group with basic operations that can be
-/// performed. This allows an FFT implementation (for example) to operate
-/// generically over either a field or elliptic curve group.
-pub trait Group: Copy + Clone + Send + Sync + 'static {
-    /// The group is assumed to be of prime order $p$. `Scalar` is the
-    /// associated scalar field of size $p$.
-    type Scalar: FieldExt;
-
-    /// Returns the additive identity of the group.
-    fn group_zero() -> Self;
-
-    /// Adds `rhs` to this group element.
-    fn group_add(&mut self, rhs: &Self);
-
-    /// Subtracts `rhs` from this group element.
-    fn group_sub(&mut self, rhs: &Self);
-
-    /// Scales this group element by a scalar.
-    fn group_scale(&mut self, by: &Self::Scalar);
-}
