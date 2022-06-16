0c159a7d5c3dc855b1bdb5caf28c36294635cd3f
yangzhou <yangzhou221@gmail.com>
2022-06-16 09:55:40 +0000

    fix compile failed

--- halo2wrong/src/lib.rs
+++ halo2wrong/src/lib.rs
@@ -1,6 +1,6 @@
 use halo2::{
     arithmetic::FieldExt,
-    circuit::{AssignedCell, Cell, Region},
+    circuit::{AssignedCell, Cell, Region, Value},
     plonk::{Advice, Column, Error, Fixed, Selector},
 };
 
@@ -25,7 +25,7 @@ impl<'a, 'b, F: FieldExt> RegionCtx<'a, 'b, F> {
         value: F,
     ) -> Result<AssignedCell<F, F>, Error> {
         self.region
-            .assign_fixed(|| annotation, column, *self.offset, || Ok(value))
+            .assign_fixed(|| annotation, column, *self.offset, || Value::known(value))
     }
 
     pub fn assign_advice(
@@ -34,11 +34,12 @@ impl<'a, 'b, F: FieldExt> RegionCtx<'a, 'b, F> {
         column: Column<Advice>,
         value: Option<F>,
     ) -> Result<AssignedCell<F, F>, Error> {
+        let v = value.ok_or(Error::Synthesis)?;
         self.region.assign_advice(
             || annotation,
             column,
             *self.offset,
-            || value.ok_or(Error::Synthesis),
+            || Value::known(v)
         )
     }
 
